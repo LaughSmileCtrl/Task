509e697c154101afc1a83d7e78a5aad4455db580
LaughSmileCtrl <firmanfardan00@gmail.com>
2020-12-05 18:10:30 +0700

    Update Pointer

--- Pointer/4.cpp
+++ Pointer/4.cpp
@@ -9,7 +9,7 @@ void urutanBulan()
     printf(" ke | Nama Bulan\n");
     for (int i = 0; i < 12; i++)
     {
-        printf(" %2d | %s\n", i+1, bulan[i]);
+        printf(" %2d | %s\n", i+1, *(bulan+i));
     }
     
 }
